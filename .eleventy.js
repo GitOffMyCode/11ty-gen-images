60079ff9b75d8a96651620aa4f897dad8a06d967
GitOffMyCode <52583811+GitOffMyCode@users.noreply.github.com>
2023-02-17 00:18:26 +0000

    Eleventy image plugin added

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,29 @@
 const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
 
+const Image = require("@11ty/eleventy-img");
+
+async function imageShortcode(src, alt, sizes) {
+  let metadata = await Image(`./src${src}`, {
+    widths: [500, 1000, null],
+    formats: ["avif", "jpeg"],
+    urlPath: "/images/",
+    outputDir: "./public/images/",
+  });
+
+  let imageAttributes = {
+    alt,
+    sizes,
+    loading: "lazy",
+    decoding: "async",
+  };
+
+  return Image.generateHTML(metadata, imageAttributes);
+}
+
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(eleventyNavigationPlugin);
 
-  eleventyConfig.addPassthroughCopy("src/assets/");
+  eleventyConfig.addPassthroughCopy("src/images/");
   eleventyConfig.addPassthroughCopy("src/css/");
 
   eleventyConfig.addWatchTarget("src/css/");
@@ -13,6 +33,8 @@ module.exports = function (eleventyConfig) {
       "src/butterflycollection/butterflies/**/*.md"
     );
   });
+
+  eleventyConfig.addNunjucksAsyncShortcode("EleventyImage", imageShortcode);
 
   return {
     markdownTemplateEngine: "njk",
